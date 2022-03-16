f6496d4d545b7b2bb33e98a3c5635d0f4be06be4
billvanleeuwen424@gmail.com <billvanleeuwen424@gmail.com>
2022-03-16 11:56:16 -0400

    Check if server and client fifo work

--- Pipe/client.c
+++ Pipe/client.c
@@ -1,4 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
+#define CLIENTFIFO "clientfifo"
+#define SERVERFIFO  "serverfifo"
+
 
 int main(){
-    
+    if(access(CLIENTFIFO, W_OK) == 0){
+        printf("yes client\n");
+    };
+    if(access(SERVERFIFO, R_OK) == 0){
+        printf("yes server\n");
+    };
 }
