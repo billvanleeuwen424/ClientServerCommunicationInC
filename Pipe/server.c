8ce22b711eca0b3e5eedb3a657cdfd3c30f66e9e
billvanleeuwen424@gmail.com <billvanleeuwen424@gmail.com>
2022-03-16 12:14:38 -0400

    changed error print

--- Pipe/server.c
+++ Pipe/server.c
@@ -1,12 +1,13 @@
 #include <sys/stat.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <errno.h>
 
 #define CLIENTFIFO "clientfifo"
 #define SERVERFIFO  "serverfifo"
 
 int main(){
-
+    
 
     /*check if can read from client, and write to server fifo*/
     if(access(CLIENTFIFO, R_OK) != 0){
@@ -14,7 +15,7 @@ int main(){
 
             
         if(mkfifo(CLIENTFIFO, S_IRUSR | S_IWUSR | S_IWGRP) == -1){  //pg 913 Linux Programming Interface
-            printf("Error building client-to-server fifo. Exiting.\n");
+            fprintf(stderr, "Error building client-to-server fifo. %d, Exiting.\n", errno);
             exit(-1);
         }
     }
@@ -23,7 +24,7 @@ int main(){
         printf("Server to Client fifo does not exist, or the permission have been configured incorrectlty. Exiting.\n");
 
         if(mkfifo(SERVERFIFO, S_IRUSR | S_IWUSR | S_IWGRP) == -1){
-            printf("Error building server-to-client fifo. Exiting.\n");
+            fprintf(stderr, "Error building server-to-client fifo. %d, Exiting.\n", errno);
             exit(-1);
         }
     }
