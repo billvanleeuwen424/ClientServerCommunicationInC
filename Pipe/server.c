c05d5e20f18768548c198ad3b23598b6b68d6844
billvanleeuwen424@gmail.com <billvanleeuwen424@gmail.com>
2022-03-21 08:33:14 -0400

    Opened the fifos

--- Pipe/server.c
+++ Pipe/server.c
@@ -8,6 +8,10 @@
 
 int main(){
     
+    int serverFileDescriptor, clientFileDescriptor;
+
+    /*clear errno*/
+    errno = 0;
 
     /*check if can read from client, and write to server fifo*/
     if(access(CLIENTFIFO, R_OK) != 0){
@@ -15,20 +19,36 @@ int main(){
 
             
         if(mkfifo(CLIENTFIFO, S_IRUSR | S_IWUSR | S_IWGRP) == -1){  //pg 913 Linux Programming Interface
-            fprintf(stderr, "Error building client-to-server fifo. %d, Exiting.\n", errno);
+            fprintf(stderr, "Error building client-to-server fifo. %s, Exiting.\n", strerror(errno));
             exit(-1);
         }
     }
-
     if(access(SERVERFIFO, W_OK) != 0){
         printf("Server to Client fifo does not exist, or the permission have been configured incorrectlty. Exiting.\n");
 
         if(mkfifo(SERVERFIFO, S_IRUSR | S_IWUSR | S_IWGRP) == -1){
-            fprintf(stderr, "Error building server-to-client fifo. %d, Exiting.\n", errno);
+            fprintf(stderr, "Error building server-to-client fifo. %s, Exiting.\n", strerror(errno));
             exit(-1);
         }
     }
 
+    /*clear errno*/
+    errno = 0;
+
+    /*open write only*/
+    serverFileDescriptor = open(SERVERFIFO, 1);
+    if(serverFileDescriptor == -1){
+        fprintf(stderr, "Error opening Server Fifo. %s", strerror(errno));
+    }
+
+    /*open read only*/
+    clientFileDescriptor = open(CLIENTFIFO, 0);
+    if(clientFileDescriptor == -1){
+        fprintf(stderr, "Error opening Client Fifo. %s", strerror(errno));
+    }
+
+
+    
     
 
     
