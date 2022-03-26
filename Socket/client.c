e1ab951751a70d983623b50ef1e24b19e665094c
billvanleeuwen424@gmail.com <billvanleeuwen424@gmail.com>
2022-03-26 15:14:20 -0400

    creating a socket in server

--- Socket/client.c
+++ Socket/client.c
@@ -2,7 +2,10 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
+#include <sys/socket.h>
 
 int main(){
-    
+
+
+
 }
--- Socket/server.c
+++ Socket/server.c
@@ -2,7 +2,32 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/ip.h>
 
 int main(){
-    
+
+    //define socket
+    int socketfd;
+    struct sockaddr_in server;
+    server.sin_family = AF_INET;
+    server.sin_port = htons(57505); //50000 + last 4 of student num
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    struct sockaddr_in client;
+    int sizeclient = sizeof(client);
+
+    //create socket
+    socketfd = socket(AF_UNIX, SOCK_STREAM, 0);
+
+    //bind and listen
+    bind(socketfd, &server, sizeof(server));
+    listen(socketfd, 1);
+
+    //wait for connection
+    int clientfd = accept(socketfd, &client, sizeclient);
+
+
+
 }
